a7b14a0db231fa3d3f9d337f8ad0e8e6ca641d64
Miguel Angel Mazzitelli <migue.mazzitelli@hotmail.com>
2024-05-03 21:50:46 -0300

    Entrega 2
    El carrito funciona estando abierto.
    
    Desde el boton add to cart de cada producto se van sumando al carrito y al cerrar el carrito no queda guardado.
    
    Faltan varias cosas, como sumar y restar productos desde el carrito y que quede guardado el carrito. Tambien quedo medio mal el maquetado y todavia no es muy responsive.

--- main.js
+++ main.js
@@ -1,7 +1,12 @@
-let iconCart = document.querySelector('.carrito');
+let iconCart = document.querySelector('.cart');
 let closeCart = document.querySelector('.close');
 let body = document.querySelector('body');
-let listProductHTML = document.querySelector('.container')
+let listProductHTML = document.querySelector('.listProduct')
+let listCartHTML = document.querySelector('.listCart');
+let iconCartSpan = document.querySelector('cart span')
+
+let listProducts = [];
+let carts = [];
 
 iconCart.addEventListener('click', () => {
     body.classList.toggle('showCart')
@@ -10,3 +15,87 @@ closeCart.addEventListener('click', () => {
     body.classList.toggle('showCart')
 })
 
+const addDataToHTML = () => {
+        listProductHTML.innerHTML = ' ';
+        if(listProducts.length > 0){
+            listProducts.forEach(product => {
+                let newProduct = document.createElement('div');
+                newProduct.dataset.id = product.id;
+                newProduct.classList.add('item');
+                newProduct.innerHTML = 
+                `<img src="${product.image}" alt="">
+                <h2>${product.name}</h2>
+                <div class="price">$${product.price}</div>
+                <button class="addCart">Add To Cart</button>`;
+                listProductHTML.appendChild(newProduct);
+            });
+        }
+    }
+listProductHTML.addEventListener('click', (event) => {
+    let positionClick = event.target;
+    if(positionClick.classList.contains('addCart')){
+        let product_id = positionClick.parentElement.dataset.id;
+            addtoCart(product_id);
+        }
+    })
+
+const addtoCart = (product_id) => {
+    let positionThisProductInCart = carts.findIndex((value) => value.product_id == product_id);
+    if(carts.length <= 0){
+        carts = [{
+            product_id: product_id,
+            quantity: 1
+        }]
+    }else if(positionThisProductInCart < 0){
+        carts.push({
+            product_id: product_id,
+            quantity: 1
+        });
+    }else{
+        carts[positionThisProductInCart].quantity = carts[positionThisProductInCart].quantity + 1;
+    }
+    addCartToHTML();
+}
+
+const addCartToHTML = () => {
+    listCartHTML.innerHTML = '';
+    let totalQuantity = 0;
+    if(carts.length > 0){
+        carts.forEach (cart => {
+            totalQuantity = totalQuantity + cart.quantity;
+            let newCart = document.createElement('div');
+            newCart.classList.add('item');
+            let positionProduct = listProducts.findIndex((value) => value.id == cart.product_id);
+            let info = listProducts[positionProduct];
+            newCart.innerHTML = `
+            <div class="image">
+                    <img src="${info.image}" alt="">
+                </div>
+                <div class="name">
+                ${info.name}
+                </div>
+                <div class="precio">
+                ${info.price * cart.quantity}
+                </div>
+                <div class="quantity">
+                    <span class="menos"> - </span>
+                    <span>${cart.quantity}</span>
+                    <span class="mas"> + </span>
+                </div>
+            `;
+        listCartHTML.appendChild(newCart);
+        })
+    }
+    iconCartSpan.innerText = totalQuantity;
+}
+
+const initApp = () => {
+
+    fetch('products.json')
+    .then(response => response.json())
+    .then(data => {
+        listProducts = data;
+        addDataToHTML();
+    })
+}
+initApp(); 
